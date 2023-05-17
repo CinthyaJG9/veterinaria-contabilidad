df42c57d6874fcf88816ea8705cd195f7ac5e94d
DiegoPonzio <druizp2000@alumno.ipn.mx>
2023-05-17 10:18:44 -0600

    Styles colores

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ['./index.jsx', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        green:
+        {
+          10: '#6ED4A5'
+        }
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 };
