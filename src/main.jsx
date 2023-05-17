f2c1937b5190574a536d670eacb68fd47b8e27a8
DiegoPonzio <druizp2000@alumno.ipn.mx>
2023-05-17 10:21:25 -0600

    Add libro

--- src/main.jsx
+++ src/main.jsx
@@ -2,49 +2,61 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App';
-import { AddServices, FixedCosts, SalePoint, Storage, PrincipalStorage, Supplier, NewLot } from './pages';
+import {
+  AddServices,
+  FixedCosts,
+  SalePoint,
+  Storage,
+  PrincipalStorage,
+  Supplier,
+  NewLot
+} from './pages';
 import './index.css';
 import AddProduct from './components/AddProduct';
-
 import { BASE_URL_API, axiosError, axiosSuccess } from '@/libs';
+import Libro from '@/pages/Libro/index.jsx';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <App />,
+    element: <App />
   },
   {
     path: '/GastosFijos',
-    element: <FixedCosts />,
+    element: <FixedCosts />
   },
   {
     path: '/Inventario',
-    element: <Storage />,
+    element: <Storage />
   },
   {
     path: '/AddProduct',
-    element: <AddProduct />,
+    element: <AddProduct />
   },
   {
     path: '/PuntoVenta',
-    element: <SalePoint />,
+    element: <SalePoint />
   },
   {
     path: '/Servicios',
-    element: <AddServices />,
+    element: <AddServices />
   },
   {
     path: '/Inicio',
-    element: <PrincipalStorage />,
+    element: <PrincipalStorage />
   },
   {
     path: '/Proveedor',
-    element: <Supplier />,
+    element: <Supplier />
   },
   {
     path: '/NuevoLote',
-    element: <NewLot />,
+    element: <NewLot />
   },
+  {
+    path: '/Libro',
+    element: <Libro />
+  }
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
