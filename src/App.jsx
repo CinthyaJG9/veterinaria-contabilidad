d0ea7dc06c1397d0514e39dc04c379503f10cf30
vDemian0w00 <cfajardoa2000@alumno.ipn.mx>
2023-05-29 00:25:12 -0600

    Dashboard MERGE DONE

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
+import Dashboard from './pages/Dashboard/index.jsx';
 import './App.css';
 
 function App() {
@@ -33,4 +34,4 @@ function App() {
   );
 }
 
-export default App;
+export default Dashboard;
